deb72418d4030d7fdd092e7664c7b21842c89dde
Jin An <jin.an@dtcc.edu>
2024-11-25 15:25:39 -0500

    Update TreeMap.java

--- Mod10AssignmentStarter/TreeMap.java
+++ Mod10AssignmentStarter/TreeMap.java
@@ -30,8 +30,9 @@ public class TreeMap<K extends Comparable<K>, V> implements Map<K, V> {
 
 class Pokemon implements Comparable<Pokemon> {
     int attack;
+    String name;
 
-    public Pokemon(int attack) {
+    public Pokemon(String name, int attack) {
         this.attack = attack;
     }
 
@@ -46,11 +47,35 @@ class Pokemon implements Comparable<Pokemon> {
 
 class Driver {
     public static void main(String[] args) {
-        Pokemon p1 = new Pokemon(500);
-        Pokemon p2 = new Pokemon(600);
+        Pokemon p1 = new Pokemon("Pikachu", 500);
+        Pokemon p2 = new Pokemon("Charizard", 600);
+        Pokemon p3 = new Pokemon("Squirtle", 400);
+        Pokemon p4 = new Pokemon("Butterfree", 200);
 
         Map<String, Pokemon> pokedex = new TreeMap<>();
         pokedex.put("Pikachu", p1);
         pokedex.put("Charizard", p2);
+        pokedex.put("Squirtle", p3);
+        pokedex.put("Butterfree", p4);
+
+
+        // Since the pokemon were added according to their name (String)
+        // the BST will be ordered in this way
+        //                    Pikachu (root)
+        //                   /       \
+        //             Charizard      Squirtle
+        //                /
+        //           Butterfree
+
+        // this should assign the Butterfree Pokemon object to ref var p5
+        // and also display the value 3, because it took 3 comparisons to retrieve
+        // the object
+        Pokemon p5 = pokedex.get("Butterfree");
+
+        // this should assign the Pikachu Pokemon object to ref var p6
+        // and also display the value 1, because it took 1 comparison to retrieve
+        // the object (which was found at the root)
+        Pokemon p6 = pokedex.get("Pikachu");
+
     }
 }
